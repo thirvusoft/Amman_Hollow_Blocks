e8e1ec0282732ae4ffe40cf179174bc4320602e6
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-05-03 12:02:03 +0530

    transaction details table added in sales invoice

--- hollow_blocks/hooks.py
+++ hollow_blocks/hooks.py
@@ -11,8 +11,8 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/hollow_blocks/css/hollow_blocks.css"
-# app_include_js = "/assets/hollow_blocks/js/hollow_blocks.js"
+app_include_css = ["holllow_blocks.bundlescss"]
+app_include_js = ["hollow_blocks.bundle.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/hollow_blocks/css/hollow_blocks.css"
